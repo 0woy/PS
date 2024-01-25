b7ab60e518c36bb5dbaab63b0c02e3e0dd418d8b
0woy <qkrdbsdk1030@naver.com>
2024-01-25 18:53:45 +0900

    1로 만들기 2 (효율 코드, 참고)

--- src/Algorithm/DP/N_12852.java
+++ src/Algorithm/DP/N_12852.java
@@ -1,5 +1,7 @@
 package Algorithm.DP;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,74 +12,41 @@ public class N_12852 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        int [] dp = new int [n+1];
-        String [] sol= new String[n+1];
+        int [] dp = new int [n+3];
 
-        switch (n){
-            case 1:
-                dp[1] =0;
-                sol[1]= "1";
-                break;
-            case 2:
-                dp[2]=1;
-                sol[2]="1 2";
-                break;
-            case 3:
-                dp[3]=1;
-                sol[3] ="1 3";
-                break;
+        dp[1] =0;
+        dp[2] =1;
+        dp[3]=1;
+
+        for(int i=4;i<=n;i++){
+            int tmp = Integer.MAX_VALUE;
+            if(i%2==0) tmp = Math.min(dp[i/2], tmp);
+            if(i%3==0) tmp = Math.min(dp[i/3], tmp);
+            tmp = Math.min(dp[i-1],tmp);
+            dp[i] = tmp+1;
         }
+            sb.append(dp[n]+"\n");
+        while (true){
+            sb.append(n+" ");
+            int tmp = Integer.MAX_VALUE;
+            int idx=-1;
+            if(n==1) break;
 
-        if(n>3) {
-            dp[1] =0;
-            sol[1]= "1";
-            dp[2] =1;
-            sol[2]="1 2";
-            dp[3] =1;
-            sol[3] ="1 3";
-            for (int i = 4; i <= n; i++) {
-                if(i%3==0&&i%2!=0) {
-                    if(dp[i/3]<dp[i-1]){
-                        dp[i] = dp[i/3]+1;
-                        sol[i] = sol[i/3]+" "+i;
-                    }
-                    else{
-                        dp[i]=dp[i-1]+1;
-                        sol[i]=sol[i-1]+" "+i;
-                    }
-                }
-                else if(i%3==0&&i%2==0){
-                    if(dp[i/3]<dp[i/2]){
-                        dp[i] = dp[i/3]+1;
-                        sol[i] = sol[i/3]+" "+i;
-                    }
-                    else{
-                        dp[i]=dp[i/2]+1;
-                        sol[i]=sol[i/2]+" "+i;
-                    }
-                }
-
-                else if(i%2==0){
-                    if(dp[i/2]<dp[i-1]){
-                        dp[i] = dp[i/2]+1;
-                        sol[i] = sol[i/2]+" "+i;
-                    }
-                    else{
-                        dp[i]=dp[i-1]+1;
-                        sol[i]=sol[i-1]+" "+i;
-                    }
-                }
-                else{
-                    dp[i]=dp[i-1]+1;
-                    sol[i] = sol[i-1]+" "+i;
+            if(n%2==0){
+                if(dp[n/2]<tmp){
+                    tmp = dp[n/2];
+                    idx = n/2;
                 }
             }
-        }
-        String [] res = sol[n].split(" ");
-        for(int i =res.length-1;i>=0;i--)
-            sb.append(res[i]+ " ");
-
-        System.out.println(dp[n]);
+            if(n%3==0){
+                if(dp[n/3]<tmp){
+                    tmp = dp[n/3];
+                    idx =n/3;
+                }
+            }
+            if(dp[n-1]<tmp) idx =n-1;
+            n = idx;
+            }
         System.out.println(sb);
         }
     }
