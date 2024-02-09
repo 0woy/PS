795cf46cfcd421ade173b384c489b58a44f7652d
0woy <qkrdbsdk1030@naver.com>
2024-02-09 18:02:12 +0900

    영역 구하기 (시간 단축)

--- src/Algorithm/BFS/N_2583.java
+++ src/Algorithm/BFS/N_2583.java
@@ -3,105 +3,76 @@ package Algorithm.BFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.*;
 
-import static Algorithm.BFS.N_1012.visitied;
 
 public class N_2583 {
-    static int [][] square;
-    static boolean [][] visited;
-    static LinkedList<Integer> que;
-    static int count =0;
-
+    static int[][] square;
+    static int height;
+    static int weight;
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb =new StringBuilder();
 
-        int height=Integer.parseInt(st.nextToken());
-        int weight=Integer.parseInt(st.nextToken());
+         height=Integer.parseInt(st.nextToken());
+         weight=Integer.parseInt(st.nextToken());
         int total=Integer.parseInt(st.nextToken());
-        visited = new boolean[height+2][weight+2];
-        square = new int[height+2][weight+2];
-
-        for(int i=0, j=0;i<height+2;i++){
-            square[i][0] = 1;
-            square[i][weight+1] = 1;
-        }
-
-        for(int i=0;i<weight+2;i++){
-            square[0][i] = 1;
-            square[height+1][i] = 1;
-        }
-
-        for(int k=0;k<total;k++){
+        square = new int[height][weight];
+        for(int i=0;i<total;i++){
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            for(int j=y1;j<y2;j++) {
-                for (int i = x1 + 1; i <= x2; i++)
-                    square[height - j][i] = 1;
+            for(int n=y1;n<y2;n++){
+                for(int m=x1;m<x2;m++)
+                    square[n][m] =1;
             }
         }
 
-        que = new LinkedList<>();
-        int size =0;
-        for(int i=1;i<=height;i++){
-            for(int j=1;j<=weight;j++){
-                if(!visited[i][j]&&square[i][j]==0) {
-                    size = bfs(square, i, j);
-                    sb.append(size+" ");
+        int count =0;
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<height;i++){
+            for(int j=0;j<weight;j++){
+                if(square[i][j]==0){
+                    list.add(bfs(i,j));
+                    count++;
                 }
             }
         }
-        st = new StringTokenizer(sb.toString());
-        int [] sizes = new int[st.countTokens()];
-        for(int i=0;i<sizes.length;i++)
-            sizes[i] = Integer.parseInt(st.nextToken());
+        Collections.sort(list);
+        sb.append(count+"\n");
+        for(int i=0;i<list.size();i++)
+            sb.append(list.get(i)+" ");
 
-        Arrays.sort(sizes);
-        sb = new StringBuilder();
-        for(int i=0;i<sizes.length;i++)
-            sb.append(sizes[i]+" ");
-
-        System.out.println(count+"\n"+sb);
+        System.out.println(sb);
     }
-    static int bfs(int[][] arr , int x, int y){
-        int size =0;
-        que.add(x);
-        que.add(y);
-        visited[x][y] = true;
-        while(que.size()!=0){
-            size++;
-            int x1 = que.poll();
-            int y1 = que.poll();
-            if(!visited[x1-1][y1]&&arr[x1-1][y1]==0 ){
-                que.add(x1-1);
-                que.add(y1);
-                visited[x1-1][y1]=true;
-            }
-            if(!visited[x1+1][y1]&&arr[x1+1][y1]==0){
-                que.add(x1+1);
-                que.add(y1);
-                visited[x1+1][y1]=true;
-            }
-            if(!visited[x1][y1-1]&&arr[x1][y1-1]==0){
-                que.add(x1);
-                que.add(y1-1);
-                visited[x1][y1-1]=true;
-            }
-            if(!visited[x1][y1+1]&&arr[x1][y1+1]==0){
-                que.add(x1);
-                que.add(y1+1);
-                visited[x1][y1+1]=true;
+
+    public static int bfs(int i, int j){
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] {i,j});
+        square[i][j] = -1; // visited 대용
+
+        int area = 1;
+        int[]dx = {0,1,0,-1};
+        int[]dy = {1,0,-1,0};
+
+        while(queue.size()>0){
+            int [] tmp = queue.poll();
+            for(int d=0;d<4;d++){
+                int nx = tmp[0]+dx[d];
+                int ny = tmp[1]+dy[d];
+
+                if(nx<0||ny<0||nx>height-1||ny>weight-1) continue;
+                if(square[nx][ny]==0){
+                    queue.offer(new int[]{nx,ny});
+                    square[nx][ny] = -1;
+                    area++;
+                }
             }
         }
-        count++;
-        return size;
+        return area;
     }
 }
