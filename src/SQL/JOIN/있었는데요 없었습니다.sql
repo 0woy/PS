250988e5ad5688ef0ce6651186cb80d82bf5d1d2
0woy <qkrdbsdk1030@naver.com>
2024-02-28 13:19:56 +0900

    있었는데요 없었습니다

---
+++ src/SQL/JOIN/있었는데요 없었습니다.sql
@@ -0,0 +1,11 @@
+/*
+ 관리자의 실수로 일부 동물의 입양일이 잘못 입력되었습니다.
+ 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL문을 작성해주세요.
+ 이때 결과는 보호 시작일이 빠른 순으로 조회해야합니다.
+ */
+
+SELECT ins.animal_id, ins.name
+from animal_ins ins inner join animal_outs outs
+                               on ins.animal_id = outs.animal_id
+where ins.datetime > outs.datetime  // 입양일이 보호 시작일 보다 빠른 tuple
+order by ins.datetime
